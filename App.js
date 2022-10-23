c66fcf88c03b738dc4eddbd88bf97cdc8c929122
Nallekari <tuomas.nallikari@outlook.com>
2022-10-23 22:37:04 +0300

    stylish tehtävä

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput, FlatList, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList } from 'react-native';
 import { AsyncStorage } from '@react-native-async-storage/async-storage';
-import * as SQLite from'expo-sqlite';
+import * as SQLite from 'expo-sqlite';
+import { Header, Icon, Input, Button, ListItem   } from'react-native-elements';
 
 export default function App() {
 
@@ -42,36 +43,27 @@ export default function App() {
   }
 
 
-  const listSeparator = () => {
-    return (
-      <View
-        style={{
-          height: 5,
-          width: "80%",
-          backgroundColor: "#fff",
-          marginLeft: "10%"
-        }}
-      />
-    );
-  };
-
   return (
     <View style={styles.container}>
-      <TextInput placeholder='Product' style={styles.typebox}
-        onChangeText={(product) => setProduct(product)}
-        value={product}/>  
-      <TextInput placeholder='Amount' style={styles.typebox}
-        onChangeText={(amount) => setAmount(amount)}
-        value={amount}/>      
-      <Button onPress={saveItem} title="Save" /> 
-      <Text style={{marginTop: 30, fontSize: 20}}>Shopping list</Text>
+      <Header
+        centerComponent={{ text: 'SHOPPING LIST', style: { color: '#fff' } }}
+      />
+      <Input   placeholder='PRODUCT' label='Product'  onChangeText={product => setProduct(product)} value={product} />
+      <Input   placeholder='AMOUNT' label='Amount'  onChangeText={amount => setAmount(amount)} value={amount} />
+      <Button raised icon={{name: 'save', color: 'white'}} onPress={saveItem} title="SAVE" /> 
       <FlatList 
-        style={{marginLeft : "5%"}}
-        keyExtractor={item => item.id.toString()} 
-        renderItem={({item}) => <View style={styles.listcontainer}><Text style={{fontSize: 18}}>{item.product}, {item.amount}</Text>
-        <Text style={{fontSize: 18, color: '#0000ff'}} onPress={() => deleteItem(item.id)}> bought</Text></View>} 
+        style={{width: '100%'}}
         data={products} 
-        ItemSeparatorComponent={listSeparator} 
+        keyExtractor={item => item.id.toString()} 
+        renderItem={({ item }) =>
+          <ListItem bottomDivider>
+            <ListItem.Content>
+              <ListItem.Title>{item.product}</ListItem.Title>
+              <ListItem.Subtitle>{item.amount}</ListItem.Subtitle>
+            </ListItem.Content>
+            <Button  type='clear' icon={{ name: 'delete', color: 'red', backgroundColor:'white'}} onPress={() => deleteItem(item.id)} title="" /> 
+          </ListItem>
+        }
       />      
     </View>
   );
@@ -80,11 +72,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 20,
-    padding: 40
+    marginTop: 40
   },
   typebox: {
     width: 200,
@@ -93,7 +85,6 @@ const styles = StyleSheet.create({
   },
   listcontainer: {
     flexDirection: 'row',
-    backgroundColor: '#fff',
     alignItems: 'center'
    },
   wordBold: {
